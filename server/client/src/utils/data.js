7759c13eaf08e5ded6e112867cb896389b13bdb5
Neeraj 2 <124696139+sujal-07-Ronny@users.noreply.github.com>
2025-05-28 15:06:38 +0530

    Update data.js

--- server/client/src/utils/data.js
+++ server/client/src/utils/data.js
@@ -1,1 +1,2 @@
-export const USER_API_ENDPOINT = "https://image-ify-m3tj.vercel.app";
+export const USER_API_ENDPOINT = "https://image-ify-m3tj.vercel.app/api/users";
+export const IMAGE_API_ENDPOINT = "https://image-ify-m3tj.vercel.app/api/images";
